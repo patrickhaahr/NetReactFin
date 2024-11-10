05c03673a5f3162b03ba4c8f087b3f02de709193
svaki <patrickqhaahr@gmail.com>
2024-11-10 22:13:25 +0100

    Add frontend files to main repository as regular folder

---
+++ frontend/src/api.tsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+import { CompanyProfile, CompanySearch } from "./company";
+
+interface SearchResponse {
+  data: CompanySearch[];
+}
+
+export const searchCompanies = async (query: string) => {
+  try {
+    const data = await axios.get<SearchResponse>(
+      `https://financialmodelingprep.com/api/v3/search-ticker?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    if (error?.response) {
+      console.log("error message: ", error.message);
+      return error.message;
+    } else {
+      console.log("unexpected error: ", error);
+      return "An unexpected error occurred.";
+    }
+  }
+};
+
+export const getCompanyProfile = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyProfile[]>(
+      `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message: ", error.message);
+  }
+};
