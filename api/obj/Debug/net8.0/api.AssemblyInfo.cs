bce5845863c3ca66b9c9b99b0ad6c099ee57b18e
patrickhaahr <patrickqhaahr@gmail.com>
2024-11-14 10:29:07 +0100

    Added PUT (Update) in stocks

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eecea5a891452f8c4a6bc2c90705322a79065622")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9615ad994d83ccf20f29ced2359185a770e46087")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
